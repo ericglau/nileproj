9447b70fc596355b5bba9ddd6df5973f7b0b99e5
Eric Lau <ericglau@outlook.com>
2022-09-15 17:52:28 -0400

    Base setup for cairo 0.9.1

---
+++ scripts/run.py
@@ -0,0 +1,24 @@
+
+import asyncio
+
+def run(nre):
+    print('from run')
+
+    classhash = nre.declare("contract", alias="contract")
+    print(classhash)
+
+
+    # address, abi = nre.declare("contract", alias="my_contract")
+    # print(abi, address)
+
+    #print(f'result ${nre.greet(["1", "2"])}')
+
+    # loop = asyncio.get_event_loop()
+    # coroutine = nre.greet(["contract"])
+    # loop.run_until_complete(coroutine)
+    
+    print('done')
+
+    #nre = NileRuntimeEnvironment()
+    #ret = nre.greet(["1", "2"])
+    # ret is equal to 3

ca72bcd6a9f13df6f06f25adf06ce685b8e8f728
Eric Lau <ericglau@outlook.com>
2022-09-21 10:49:06 -0400

    Store impl abi with proxy deployment

--- scripts/run.py
+++ scripts/run.py
@@ -4,15 +4,15 @@ def run(nre):
 
     addr = nre.deploy_proxy(["contract"])
 
-    nre.invoke(addr, "increase_balance", params=['1'], abi='artifacts/abis/contract.json')
-    print(f"balance: {nre.call(addr, 'get_balance', abi='artifacts/abis/contract.json')}")
+    nre.invoke(addr, "increase_balance", params=['1'])
+    print(f"balance: {nre.call(addr, 'get_balance')}")
 
     print('Testing upgrade proxy:')
 
     nre.upgrade_proxy([addr, "contract_v2"])
 
-    print(f"balance: {nre.call(addr, 'get_balance', abi='artifacts/abis/contract_v2.json')}")
-    nre.invoke(addr, "reset_balance", abi='artifacts/abis/contract_v2.json')
-    print(f"balance: {nre.call(addr, 'get_balance', abi='artifacts/abis/contract_v2.json')}")
+    print(f"balance: {nre.call(addr, 'get_balance')}")
+    nre.invoke(addr, "reset_balance", override_abi='artifacts/abis/contract_v2.json')
+    print(f"balance: {nre.call(addr, 'get_balance')}")
     
     print('Done test')

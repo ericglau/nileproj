e0f5eece6946562040ea46d0e3371094be142741
Eric Lau <ericglau@outlook.com>
2022-09-27 15:38:24 -0400

    Use accounts WIP

--- scripts/run.py
+++ scripts/run.py
@@ -1,12 +1,16 @@
 
+from nile.core.account import Account
+
 def run(nre):
     addr = nre.deploy_proxy(["contract"])
 
-    nre.invoke(addr, "increase_balance", params=['1'])
+    account = Account('PKEY1', nre.network)
+
+    account.send(addr, "increase_balance", calldata=['1'], max_fee=0)
     print(f"balance from v1: {nre.call(addr, 'get_balance')}")
 
     nre.upgrade_proxy([addr, "contract_v2"])
     print(f"balance from v2: {nre.call(addr, 'get_balance')}")
 
-    nre.invoke(addr, "reset_balance")
+    account.send(addr, "reset_balance", calldata=[], max_fee=0)
     print(f"balance after reset from v2: {nre.call(addr, 'get_balance')}")

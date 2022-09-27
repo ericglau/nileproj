1725da3d493c8d484aa8cadc014e8d49c0b7f7c5
Eric Lau <ericglau@outlook.com>
2022-09-27 16:12:37 -0400

    Pass in signer

--- scripts/run.py
+++ scripts/run.py
@@ -2,14 +2,14 @@
 from nile.core.account import Account
 
 def run(nre):
-    addr = nre.deploy_proxy(["contract"])
-
     account = Account('PKEY1', nre.network)
+
+    addr = nre.deploy_proxy(["contract", account.alias])
 
     account.send(addr, "increase_balance", calldata=['1'], max_fee=0)
     print(f"balance from v1: {nre.call(addr, 'get_balance')}")
 
-    nre.upgrade_proxy([addr, "contract_v2"])
+    nre.upgrade_proxy([addr, "contract_v2", account.alias])
     print(f"balance from v2: {nre.call(addr, 'get_balance')}")
 
     account.send(addr, "reset_balance", calldata=[], max_fee=0)

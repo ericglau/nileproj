6ed86e5aabee2e886c6415fd67f00b01a8007221
Eric Lau <ericglau@outlook.com>
2022-09-27 17:23:58 -0400

    Pass max_fee arg

--- scripts/run.py
+++ scripts/run.py
@@ -4,12 +4,12 @@ from nile.core.account import Account
 def run(nre):
     account = Account('PKEY1', nre.network)
 
-    addr = nre.deploy_proxy(["contract", account.alias])
+    addr = nre.deploy_proxy(["contract", account.alias, '0'])
 
     account.send(addr, "increase_balance", calldata=['1'], max_fee=0)
     print(f"balance from v1: {nre.call(addr, 'get_balance')}")
 
-    nre.upgrade_proxy([addr, "contract_v2", account.alias])
+    nre.upgrade_proxy([addr, "contract_v2", account.alias, '0'])
     print(f"balance from v2: {nre.call(addr, 'get_balance')}")
 
     account.send(addr, "reset_balance", calldata=[], max_fee=0)

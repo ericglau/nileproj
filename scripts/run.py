be8ca1c89080da3fdd1f5a4868c8166d444fb977
Eric Lau <ericglau@outlook.com>
2022-09-27 17:46:35 -0400

    Make max_fee optional

--- scripts/run.py
+++ scripts/run.py
@@ -4,13 +4,13 @@ from nile.core.account import Account
 def run(nre):
     account = Account('PKEY1', nre.network)
 
-    addr = nre.deploy_proxy(["contract", account.alias, '0'])
+    addr = nre.deploy_proxy(["contract", account.alias]) # optional args: "--max_fee", 0
 
-    account.send(addr, "increase_balance", calldata=['1'], max_fee=0)
+    account.send(addr, "increase_balance", calldata=['1'])
     print(f"balance from v1: {nre.call(addr, 'get_balance')}")
 
-    nre.upgrade_proxy([addr, "contract_v2", account.alias, '0'])
+    nre.upgrade_proxy([addr, "contract_v2", account.alias]) # optional args: "--max_fee", 0
     print(f"balance from v2: {nre.call(addr, 'get_balance')}")
 
-    account.send(addr, "reset_balance", calldata=[], max_fee=0)
+    account.send(addr, "reset_balance", calldata=[])
     print(f"balance after reset from v2: {nre.call(addr, 'get_balance')}")

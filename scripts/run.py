313e0cbaf3e21c544a1aaa62413700e5da8de9ca
Eric Lau <ericglau@outlook.com>
2022-09-27 18:18:58 -0400

    Pass admin to initializer args

--- scripts/run.py
+++ scripts/run.py
@@ -4,7 +4,7 @@ from nile.core.account import Account
 def run(nre):
     account = Account('PKEY1', nre.network)
 
-    addr = nre.deploy_proxy(["contract", account.alias]) # optional args: "--max_fee", 0
+    addr = nre.deploy_proxy(["contract", account.alias, account.address]) # optional args: "--max_fee", 0
 
     account.send(addr, "increase_balance", calldata=['1'])
     print(f"balance from v1: {nre.call(addr, 'get_balance')}")

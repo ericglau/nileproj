2ffccc00edfdc346cebff2c6fb37deab9036c6ca
Eric Lau <ericglau@outlook.com>
2022-10-24 10:59:15 -0400

    Update for nile main

--- scripts/run.py
+++ scripts/run.py
@@ -1,16 +1,18 @@
 
 from nile.core.account import Account
+from nile.utils import hex_address
 
 def run(nre):
     signer = "PKEY1"
     account = Account(signer, nre.network)
 
-    proxy = nre.deploy_proxy([signer, "contract", account.address]) # optional args: "--max_fee", 0
+    proxy = nre.deploy_proxy([signer, "contract", hex_address(account.address)])
 
     account.send(proxy, "increase_balance", calldata=['1'])
     print(f"balance from v1: {nre.call(proxy, 'get_balance')}")
 
-    nre.upgrade_proxy([signer, proxy, "contract_v2"]) # optional args: "--max_fee", 0
+    tx = nre.upgrade_proxy([signer, hex_address(proxy), "contract_v2"])
+    print(f"upgrade tx: {tx}")
     print(f"balance from v2: {nre.call(proxy, 'get_balance')}")
 
     account.send(proxy, "reset_balance", calldata=[])
